25b405e71bd6ec7baffcea5de43f9a162a1f1fb6
Alien Deployer <m@a17.name>
2024-05-21 23:38:01 +0300

    add vars default values

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,18 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const DEFAULT_PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     polygon: {
       url: "https://polygon-rpc.com",
-      accounts: [vars.get("PRIVATE_KEY"),],
+      accounts: [vars.get("PRIVATE_KEY", DEFAULT_PRIVATE_KEY),],
     },
   },
   etherscan: {
-    apiKey: vars.get("ETHERSCAN_API_KEY"),
+    apiKey: vars.get("ETHERSCAN_API_KEY", ""),
   },
 };
 
